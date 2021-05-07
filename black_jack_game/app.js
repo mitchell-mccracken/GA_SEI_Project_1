cfce6b8f4ddf48dcc158890d5579c0abd9caeb39
mitchell-mccracken <mpm5069@gmail.com>
2021-05-07 14:38:13 -0400

    updated deckOfCards to be an array of objects

--- black_jack_game/app.js
+++ black_jack_game/app.js
@@ -2,21 +2,60 @@
 const print = (input) => { console.log(input) };
 print('js file connected')
 
-// ====================  create array for the deck of cards, only using 1 deck of cards at first, format will be face value '-' suit value
+// ====================  variables to create deck of cards array
 const deckOfCards = [];
-const cardNumberArray = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k', 'a'];
+const cardNumberArray = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
 const cardSuitArray = [ 'heart' , 'diamond' , 'spade' , 'club'];
+const dealerHand = [];
+const playerHand = [];
 
+// ==================== function to determine point value
+const pointValue = (faceValue) => {
+    if (faceValue === 'ace') {
+        return 11;
+    } else if (faceValue === cardNumberArray[9] || faceValue === cardNumberArray[10] || faceValue === cardNumberArray[11] ) {
+        return 10;
+    } else {
+        return faceValue;
+    }
+}
+
+// =================== function to determine color
+const cardColor = (suitValue) => {
+    if (suitValue === 'heart' || suitValue === 'diamond') {
+        return 'red';
+    } else {
+        return 'black';
+    }
+}
+
+// ========== create class to generate card object
+class CardGeneration {
+    constructor (faceValue , suitValue, color) {
+        this.faceValue = faceValue;
+        this.suitValue = suitValue;
+        this.pointValue = pointValue(faceValue);
+        this.color = cardColor(suitValue);
+    }
+}
+
+// =========== generate deck of cards
 for (number of cardNumberArray) {
     let num = number;
     for (suit of cardSuitArray) {
         let cardSuit = suit;
-        deckOfCards.push(`${num}-${cardSuit}`);
+        // deckOfCards.push(`${num}-${cardSuit}`);
+        const card = new CardGeneration( num , cardSuit , 'nocolor');
+        deckOfCards.push(card);
     }
 }
-print(deckOfCards);
-let chosenCard = deckOfCards[0].split('-');
-print(chosenCard);
+
+// =============== function to reset the game
+const reset = () => {
+    print('game reset');
+    playerHand = [];
+    dealerHand = [];
+}
 
 // ================ random number generator based on length of deckOfCards array
 const randNum = () => {
