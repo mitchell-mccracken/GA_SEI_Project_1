2bf938edb7c7f58affc9adfacc7c990817baa7f0
mitchell-mccracken <mpm5069@gmail.com>
2021-05-10 09:08:52 -0400

    added game logic and cleaned code

--- black_jack_game/app.js
+++ black_jack_game/app.js
@@ -1,15 +1,16 @@
 //print function used in place of console.log
 const print = (input) => { console.log(input) };
-print('js file connected')
+console.log('js file connected')
 
 // ====================  variables to create deck of cards array
 const deckOfCards = [];
 const cardNumberArray = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
-const cardSuitArray = [ 'heart' , 'diamond' , 'spade' , 'club'];
+const cardSuitArray = [ 'hearts' , 'diamonds' , 'spades' , 'clubs'];
 let dealerHand = [];
 let dealerPoints = 0;
 let playerHand = [];
 let playerPoints = 0;
+let playerDecisionMade = 'not made';
 
 // ==================== function to determine point value
 const pointValue = (faceValue) => {
@@ -24,7 +25,7 @@ const pointValue = (faceValue) => {
 
 // =================== function to determine color
 const cardColor = (suitValue) => {
-    if (suitValue === 'heart' || suitValue === 'diamond') {
+    if (suitValue === 'hearts' || suitValue === 'diamonds') {
         return 'red';
     } else {
         return 'black';
@@ -33,7 +34,7 @@ const cardColor = (suitValue) => {
 
 // ========== create class to generate card object
 class CardGeneration {
-    constructor (faceValue , suitValue, color) {
+    constructor (faceValue , suitValue) {
         this.faceValue = faceValue;
         this.suitValue = suitValue;
         this.pointValue = pointValue(faceValue);
@@ -46,7 +47,6 @@ for (number of cardNumberArray) {
     let num = number;
     for (suit of cardSuitArray) {
         let cardSuit = suit;
-        // deckOfCards.push(`${num}-${cardSuit}`);
         const card = new CardGeneration( num , cardSuit , 'nocolor');
         deckOfCards.push(card);
     }
@@ -54,7 +54,7 @@ for (number of cardNumberArray) {
 
 // =============== function to reset the game
 const reset = () => {
-    print('game reset');
+    console.log('game reset');
     playerHand = [];
     dealerHand = [];
 }
@@ -62,7 +62,7 @@ const reset = () => {
 // ================ random number generator based on length of deckOfCards array
 const randNum = () => {
     let randomNumber =  Math.floor( Math.random() * deckOfCards.length );
-    print(randomNumber);
+    // console.log(randomNumber);
     return randomNumber;
 }
 
@@ -72,40 +72,104 @@ const generateCardValue = (forWho) => {
     forWho.push(card);
 }
 
-// ================= check points
-const checkScore = () => {
+// ================ calculate dealer score
+const calcDealerScore = () => {
+    dealerPoints = 0;                   // I don't think this is needed
     //check if it is the first two cards for the dealer, if so only display one of them
-    if (dealerHand.length === 2) {
+    if (dealerHand.length === 2 && playerDecisionMade === 'not made') {
         dealerPoints = dealerHand[1].pointValue;
-        print(`dealer card : ${dealerHand[1].faceValue} of ${dealerHand[1].suitValue}`);
+        console.log(`dealer card : ${dealerHand[1].faceValue} of ${dealerHand[1].suitValue}`);
+        console.log(`dealer points: ${dealerPoints}`);
     } else{
         for (i=0 ; i<dealerHand.length ; i++) {
             dealerPoints += dealerHand[i].pointValue;
-            print(`dealer card : ${dealerHand[i].faceValue} of ${dealerHand[i].suitValue}`);
+            console.log(`dealer card : ${dealerHand[i].faceValue} of ${dealerHand[i].suitValue}`);
         }
+        console.log(`dealer points: ${dealerPoints}`);
     }
-    print(`dealer points: ${dealerPoints}`);
-    print(' ------------------');
+}
+
+// ================ calculate user score
+const calcPlayerScore = () => {
+    playerPoints = 0;               //maybe not needed
     for (i=0 ; i<playerHand.length ; i++) {
         playerPoints += playerHand[i].pointValue;
-        print(`player card : ${playerHand[i].faceValue} of ${playerHand[i].suitValue}`);
+        console.log(`player card : ${playerHand[i].faceValue} of ${playerHand[i].suitValue}`);
     }
-    print(`player points: ${playerPoints}`);
+    console.log(`player points: ${playerPoints}`);
+}
+
+// ================= check score
+const checkScore = () => {
+    playerPoints = 0;               //have to reset everytime I count because this is set as a global variable
+    dealerPoints = 0;               //have to reset everytime I count because this is set as a global variable
+    console.log('========= check score ===========')
+    calcDealerScore();
+    console.log(' ------------------');
+    calcPlayerScore();
+    // check if dealer or player is > 21
+    checkForAWin();
+
+    if (playerDecisionMade === 'not made'){
+        setTimeout(playerDecision , 1000);
+    } else if (playerDecisionMade === 'made') {
+        dealerDecisionModel();
+    }
+    
     ///should probably include playerDecision in this function then allow it to loop back to this function if player hits, also may need to add a check to see if anyone has lost yet
 }
 
 // ============== player decision
 const playerDecision = () => {
     let decision = prompt('would you like to hit or stay' , 'stay');
-    print(decision);
+    console.log(decision);
+    console.log('******************')
     if (decision === 'hit') {
         generateCardValue(playerHand);
+        checkScore();
     }
     else if (decision === 'stay') {
-        //need to check score
-        //if 
+        playerDecisionMade = 'made';
+        checkScore()
+        // dealerDecisionModel();          //I dont think this is needed
+
+    } 
+    // else {
+    //     alert('please enter a valid decision');
+    //     playerDecision();
+    // }
+}
+
+// =========== the decision model for dealer getting dealer more cards 
+const dealerDecisionModel = () => {
+    if (playerDecisionMade === 'made'){      
+        if (dealerPoints < playerPoints) {
+            console.log('generating card for dealer');
+            generateCardValue(dealerHand);
+            checkScore();
+        } else if (dealerPoints > playerPoints && dealerPoints > 21) {
+            alert('*** dealer loses!');
+            return
+        } else if (dealerPoints > playerPoints && dealerPoints <= 21){
+            alert('***** dealer wins!');
+            return
+        }
     }
 }
+
+// ============= check for a win
+const checkForAWin = () => {
+    if (playerPoints > 21) {
+        alert ('dealer wins, player hand is > 21');
+        return
+    }
+    if (dealerPoints > 21) {
+        alert ('player wins, dealer hand is > 21');
+        return
+    }
+
+}
+
 
 // ============ function to start game
 const startGame = () => {
@@ -113,8 +177,8 @@ const startGame = () => {
     generateCardValue(dealerHand);
     generateCardValue(playerHand);
     generateCardValue(playerHand);
-    checkScore();
-    setTimeout(playerDecision , 2000);
+    checkScore();           //might want to hard code the dealer score check of the single card then roll into score check of player followed by player choice. player choice wll be essentially a loop until they choose stay or 5 cards are delt. After stay or 5 player cardss then it will go into the dealer loop checking the following : is dealer score? > 21 -no-> is dealer score > player score? -no-> deal card for dealer, start at the beginning of checks
+    
 }
 
 
@@ -135,14 +199,15 @@ startGame();
 // ==============================================================================
 
 // ============== function to generate a div that represents the randomly chosen card, this is for the UI portion. I will hold off on this for now.
-const generateCard = () => {
-    let divCard = document.createElement('div');
-    let divText = document.createElement('div');
-    divCard.setAttribute('class' , 'card');
-    divText.setAttribute('class' , 'card-text');
-    divText.innerText = '2 hearts';
-    divCard.appendChild(divText);
-    document.querySelector('#dealer-cards').appendChild(divCard);
+// const generateCard = () => {
+//     let divCard = document.createElement('div');
+//     let divText = document.createElement('div');
+//     divCard.setAttribute('class' , 'card');
+//     divText.setAttribute('class' , 'card-text');
+//     divText.innerText = '2 hearts';
+//     divCard.appendChild(divText);
+//     document.querySelector('#dealer-cards').appendChild(divCard);
+// }
 
-}
+
 
